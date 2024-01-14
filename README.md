efc2b91d171b0dcf650b72d95d5b05ae41ce7e8c
Juned Khan <junedkhanc101@gmail.com>
2024-01-14 20:58:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+The Mad Scientist blog is a science niche blog for nerds.
+Website is in development.
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Mad Scientist';
+export const SITE_DESCRIPTION = 'Welcome to my website! I write about science and technology from physics, astronomy to biology and geology';
